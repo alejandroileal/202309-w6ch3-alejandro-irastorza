5719fe91e8262b27b91e0a58f9851459a6a0454a
alejandroileal <aleal1202@outlook.com>
2023-11-15 18:21:36 +0100

    Add reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,17 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import charactersReducer from '../slice/characters.slice';
 
 export const store = configureStore({
   reducer: {
-    gotCharacters: 
+    gotCharacters: charactersReducer,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
