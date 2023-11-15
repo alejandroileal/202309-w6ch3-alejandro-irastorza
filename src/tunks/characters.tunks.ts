50f60eacf8195539325fde5655de11c16e101cf2
alejandroileal <aleal1202@outlook.com>
2023-11-15 17:49:28 +0100

    Add types to load character function

--- src/tunks/characters.tunks.ts
+++ src/tunks/characters.tunks.ts
@@ -1,9 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { EldenApiRepo } from '../services/eldenApiRepo';
+import { Character } from '../model/character';
 
-export const loadCharactersTunk = createAsyncThunk(
+export const loadCharactersTunk = createAsyncThunk<Character[], EldenApiRepo>(
   'characters/load',
-  async (repo: EldenApiRepo) => {
+  async (repo) => {
     const characters = await repo.getCharacters();
     return characters;
   }
