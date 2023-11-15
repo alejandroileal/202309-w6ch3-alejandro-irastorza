d475fd88983fa9e623eafc06b8172aadcfa6c90f
alejandroileal <aleal1202@outlook.com>
2023-11-15 18:03:30 +0100

    Fix generic types to load function

--- src/tunks/characters.tunks.ts
+++ src/tunks/characters.tunks.ts
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { EldenApiRepo } from '../services/eldenApiRepo';
+import { AnyCharacter, EldenApiRepo } from '../services/eldenApiRepo';
 import { Character } from '../model/character';
 
-export const loadCharactersTunk = createAsyncThunk<Character[], EldenApiRepo>(
-  'characters/load',
-  async (repo) => {
-    const characters = await repo.getCharacters();
-    return characters;
-  }
-);
+export const loadCharactersTunk = createAsyncThunk<
+  AnyCharacter[],
+  EldenApiRepo
+>('characters/load', async (repo) => {
+  const characters = await repo.getCharacters();
+  return characters;
+});
