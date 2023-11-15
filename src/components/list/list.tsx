29877f018600eb9313b008d0ba4c92dfb9c9075f
alejandroileal <aleal1202@outlook.com>
2023-11-15 18:38:23 +0100

    Add state rendering cases

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { Character } from '../../model/character';
+import React, { useEffect } from 'react';
 import { GOTCard } from '../card/got.card';
 import '../../index.css';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import { useCharacters } from '../../hooks/characters/use.characters';
 
 export function List() {
+  const { characters, state } = useSelector(
+    (state: RootState) => state.gotCharacters
+  );
+
+  const { loadCharacters } = useCharacters();
+
+  useEffect(() => {
+    loadCharacters();
+  }, [loadCharacters]);
+
+  if (state === 'loading') {
+    return <p>Loading characters...</p>;
+  }
+
+  if (state === 'error') {
+    return <p>Ups! There was an error loading characters 😔</p>;
+  }
+
   return (
     <div>
       <ul className="characters-list row list-unstyled">
